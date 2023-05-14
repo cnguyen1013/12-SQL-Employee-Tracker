a15353f0191f5285d63163ea22606e508bce5986
cnguyen1013 <CNguyen7@mdanderson.org>
2023-05-14 16:18:45 -0500

    initial push

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_manager_db;
+CREATE DATABASE employee_manager_db;
+
+USE employee_manager_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL, 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT, 
+    title VARCHAR(30) NULL,
+    salary DECIMAL NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT, 
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NOT NULL, 
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
