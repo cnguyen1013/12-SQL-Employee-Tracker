a5f922dacd8643cafa418cdf1a86f74a6a6e238d
cnguyen1013 <CNguyen7@mdanderson.org>
2023-05-14 17:25:37 -0500

    fixed multiple syntax errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,19 +7,20 @@ VALUES ("Sales"),
        ("Legal");
 
 INSERT INTO role(title, salary, department_id)
-VALUES ("Salesperson, 80000, 1"),
-       ("Lead Engineer, 150000, 2"),
-       ("Software Engineer, 120000, 2"),
-       ("Account Manager, 160000, 3"),
-       ("Accountant, 125000, 3"),
-       ("Legal Team Lead, 250000, 4"),
-       ("Lawyer, 190000, 4");
+VALUES ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Mike", "Chen", 1), 
-       ("Ashley", "Rodriguez", 2), 
-       ("Kevin", "Tupik", 2),
-       ("Kunal", "Singh", 3),
-       ("Malia", "Brown", 3),
-       ("Sarah", "Lourd", 4),
-       ("Tom", "Allen", 4)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, null),
+       ("Mike", "Chen", 2, 1), 
+       ("Ashley", "Rodriguez", 3, null), 
+       ("Kevin", "Tupik", 4, 3),
+       ("Kunal", "Singh", 5, null),
+       ("Malia", "Brown", 6, 5),
+       ("Sarah", "Lourd", 7, null),
+       ("Tom", "Allen", 8, 7);
