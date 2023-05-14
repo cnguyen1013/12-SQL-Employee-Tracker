d91655ec29ed0ad7861af119ff922099d3a213cf
cnguyen1013 <CNguyen7@mdanderson.org>
2023-05-14 18:07:58 -0500

    created foreign keys referencing primary keys

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE role (
     title VARCHAR(30) NULL,
     salary DECIMAL NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -23,5 +24,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NULL,
     role_id INT NOT NULL, 
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
